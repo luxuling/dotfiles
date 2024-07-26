58ca6bf3a69ea1bedf9f32b3c023d9348fddc5e7
Antoni Saputra <antonisaputra049@gmail.com>
2024-07-26 17:48:52 +0700

    feat: add markdown preview

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -144,4 +144,13 @@ return {
       require("telescope").load_extension("file_browser")
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
