a353d206a32dade71705bc3a28ef6abce5223c5e
Antoni Saputra <antonisaputra049@gmail.com>
2024-09-09 18:00:06 +0700

    feat: update lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -144,8 +144,8 @@ return {
 					},
 				})
 			end,
-			["tsserver"] = function()
-				lspconfig.tsserver.setup({
+			["ts_ls"] = function()
+				lspconfig.ts_ls.setup({
 					init_options = {
 						plugins = {
 							{
