57695897dd8fd6ef4178a58b4245361180935526
Antoni Saputra <antonisaputra049@gmail.com>
2024-09-23 08:40:22 +0700

    feat: update

--- nvim/.config/nvim/lua/plugins/live-preview.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"brianhuster/live-preview.nvim",
-	build = "npm init && npm install && npm install -g nodemon", --- if you use npm
-	-- build = 'yarn init && yarn install && yarn global add nodemon', --- if you use yarn
-	ft = { "html", "md", "markdown" },
-	opts = {},
-}
